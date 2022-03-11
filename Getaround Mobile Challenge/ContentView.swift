bda35381ee0b64a1b0b5275004ec627b719f9cf4
LluisArmen <armengolluis@gmail.com>
2022-03-11 15:31:30 +0100

    Initial Commit

---
+++ Getaround Mobile Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Getaround Mobile Challenge
+//
+//  Created by Lluis Armengol on 11/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Getaround Mobile Challenge/Getaround_Mobile_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Getaround_Mobile_ChallengeApp.swift
+//  Getaround Mobile Challenge
+//
+//  Created by Lluis Armengol on 11/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Getaround_Mobile_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
